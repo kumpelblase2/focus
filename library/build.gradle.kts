82028ec95583f275dd73188e36a41e929a0a9a6e
Tim Hagemann <tim@eternalwings.de>
2020-04-29 09:53:55 +0200

    Include slf4j nop library
    Since we use OSHI, which uses slf4j, we - sadly - need to provide an slf4j
    implementation. However, we don't really care about slf4j so we just include the
     nop version. Would be great if we didn't have to, but it is what it is.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -10,6 +10,9 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.github.oshi:oshi-core:4.6.1")
     compile(group = "org.jdom", name = "jdom2", version = "2.0.6")
+    // technically I don't want this, but OSHI uses it and otherwise prints an error :/
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    implementation("org.slf4j:slf4j-nop:1.7.30")
 }
 
 tasks {
