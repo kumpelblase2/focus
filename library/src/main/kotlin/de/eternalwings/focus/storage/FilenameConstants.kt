9b2a9abc202a7c1938ba72b8f17e7639776cb725
Tim Hagemann <tim@eternalwings.de>
2020-04-29 09:56:26 +0200

    Fix invalid zone information for timestamps in file names
    We didn't specify a zone information in the timestamps for changesets and client
     device files. Thus it used the local time zone, which is wrong. Instead, we
    need to specify that we want to use GMT for those as that is how OmniFocus saves
     these files.

--- library/src/main/kotlin/de/eternalwings/focus/storage/FilenameConstants.kt
+++ library/src/main/kotlin/de/eternalwings/focus/storage/FilenameConstants.kt
@@ -1,5 +1,6 @@
 package de.eternalwings.focus.storage
 
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 object FilenameConstants {
@@ -7,7 +8,8 @@ object FilenameConstants {
     const val CONTENT_FILE_NAME = "contents.xml"
     const val CLIENT_FILE_NAME = ".client"
     const val CAPABILITY_FILE_NAME = ".capability"
-    val CHANGESET_TIME_FORMAT: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
+    val CHANGESET_TIME_FORMAT: DateTimeFormatter =
+        DateTimeFormatter.ofPattern("yyyyMMddHHmmss").withZone(ZoneId.of("GMT"))
     const val CHANGESET_INIT_TIMESTAMP = "00000000000000"
     val CLIENT_FILE_DATE_FORMAT: DateTimeFormatter = CHANGESET_TIME_FORMAT
 }
