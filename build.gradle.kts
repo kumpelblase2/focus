1b14d2a1eec1194ecf0574f7ae28f08d6b03b07c
Tim Hagemann <tim@eternalwings.de>
2020-04-30 09:00:47 +0200

    Do not include all of konf library
    I assume that `konf` artifact was the "core" artifact of the library, but that
    appears to be wrong. Since we specifically already include the hocon artifact,
    we can safely switch to "konf-core" without losing any features.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     implementation("com.github.h0tk3y.betterParse:better-parse-jvm:0.4.0-alpha-3")
     implementation("com.github.ajalt:clikt:2.6.0")
     implementation("com.google.code.gson:gson:2.8.6")
-    implementation("com.uchuhimo:konf:0.22.1")
+    implementation("com.uchuhimo:konf-core:0.22.1")
     implementation("com.uchuhimo:konf-hocon:0.22.1")
 
     testCompile("org.junit.jupiter:junit-jupiter-api:5.6.2")
