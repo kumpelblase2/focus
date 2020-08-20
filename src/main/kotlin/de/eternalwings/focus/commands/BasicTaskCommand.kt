b88d5d8cc8d399ee437c37be33220a74356908cd
Tim Hagemann <tim@eternalwings.de>
2020-08-20 16:26:14 +0200

    Create base command for task related actions

---
+++ src/main/kotlin/de/eternalwings/focus/commands/BasicTaskCommand.kt
@@ -0,0 +1,50 @@
+package de.eternalwings.focus.commands
+
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.option
+import de.eternalwings.focus.ErrorCodes
+import de.eternalwings.focus.failWith
+import de.eternalwings.focus.view.OmniFocusState
+import de.eternalwings.focus.view.OmniTask
+
+abstract class BasicTaskCommand(
+    help: String = "",
+    epilog: String = "",
+    name: String? = null,
+    invokeWithoutSubcommand: Boolean = false,
+    printHelpOnEmptyArgs: Boolean = false,
+    helpTags: Map<String, String> = emptyMap(),
+    autoCompleteEnvvar: String? = "",
+    allowMultipleSubcommands: Boolean = false
+) : UnlockedStorageBasedCommand(
+    help,
+    epilog,
+    name,
+    invokeWithoutSubcommand,
+    printHelpOnEmptyArgs,
+    helpTags,
+    autoCompleteEnvvar,
+    allowMultipleSubcommands
+), WithDeviceCommand {
+
+    val id by argument(name = "task-id", help = "The ID of the task that is done")
+    override val deviceName by option(
+        "--device", "-d",
+        help = "The name of the device to use. Uses the configured device by default."
+    )
+    override val deviceId by option(
+        "--device-id", "-i",
+        help = "The id of the device to use. Uses the configured device by default."
+    )
+
+    override fun run() {
+        val storage = getUnlockedStorage()
+        val view = OmniFocusState(storage, true)
+        val task =
+            view.getTaskById(id) ?: failWith("Could not find task with id $id", ErrorCodes.GENERIC_ARGUMENT_ERROR)
+        val changedTask = applyChanges(task)
+        view.updateTask(changedTask, getDevice(storage))
+    }
+
+    abstract fun applyChanges(task: OmniTask) : OmniTask
+}
--- src/main/kotlin/de/eternalwings/focus/commands/DoTaskCommand.kt
+++ src/main/kotlin/de/eternalwings/focus/commands/DoTaskCommand.kt
@@ -1,31 +1,11 @@
 package de.eternalwings.focus.commands
 
-import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.options.option
-import de.eternalwings.focus.ErrorCodes
-import de.eternalwings.focus.failWith
-import de.eternalwings.focus.view.OmniFocusState
+import de.eternalwings.focus.view.OmniTask
 import java.time.ZonedDateTime
 
-class DoTaskCommand : UnlockedStorageBasedCommand(name = "do", help = "Marks a task as done"), WithDeviceCommand {
+class DoTaskCommand : BasicTaskCommand(name = "do", help = "Marks a task as done") {
 
-    val id by argument(name = "task-id", help = "The ID of the task that is done")
-    override val deviceName by option(
-        "--device", "-d",
-        help = "The name of the device to use. Uses the configured device by default."
-    )
-    override val deviceId by option(
-        "--device-id", "-i",
-        help = "The id of the device to use. Uses the configured device by default."
-    )
-
-    override fun run() {
-        val storage = getUnlockedStorage()
-        val view = OmniFocusState(storage, true)
-        val task =
-            view.getTaskById(id) ?: failWith("Could not find task with id $id", ErrorCodes.GENERIC_ARGUMENT_ERROR)
-
-        val doneTask = task.copy(completed = ZonedDateTime.now())
-        view.updateTask(doneTask, getDevice(storage))
+    override fun applyChanges(task: OmniTask): OmniTask {
+        return task.copy(completed = ZonedDateTime.now())
     }
 }
