bc452199f20e18b684c4607cc789bd368fd1ea0b
Tim Hagemann <tim@eternalwings.de>
2020-08-21 10:09:52 +0200

    Update to most recent kotlin version
    This requires us to update gradle to at least 6.0, however it seems the newest
    gradle version (6.5.1) doesn't mesh well with intellij.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("jvm") version "1.3.61"
+    kotlin("jvm") version "1.4.0"
     id("com.github.johnrengelman.shadow") version "4.0.4"
 }
 
